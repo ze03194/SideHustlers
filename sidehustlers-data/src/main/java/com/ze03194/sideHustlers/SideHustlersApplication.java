f1dcc2ebd35f930da413d3f7b184842eeb155ddc
Zach <z.g.elmalak@gmail.com>
2022-11-28 21:16:06 -0600

    fixing

--- sidehustlers-data/src/main/java/com/ze03194/sideHustlers/SideHustlersApplication.java
+++ sidehustlers-data/src/main/java/com/ze03194/sideHustlers/SideHustlersApplication.java
@@ -2,7 +2,6 @@ package com.ze03194.sideHustlers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,13 +9,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class SideHustlersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SideHustlersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SideHustlersApplication.class, args);
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(11);
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(11);
+    }
 
 }
